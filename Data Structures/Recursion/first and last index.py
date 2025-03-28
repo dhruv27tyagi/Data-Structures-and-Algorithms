0a80c524e3ad6ab98afe3dc9ab0a5c879425fb56
dhruv27tyagi <dhruv63tyagi@gmail.com>
2025-03-28 18:30:06 +0530

    first and last index

---
+++ Data Structures/Recursion/first and last index.py
@@ -0,0 +1,26 @@
+"""
+Given an array and an element, find 
+a. first index of element
+b. last index of element
+c. return -1 if element not in list
+"""
+
+def find_first_index(arr, element):
+    if len(arr) == 0:
+        return -1
+    
+    if(arr[0]==element):
+        return 0
+    
+    ansfromrecursion =  find_first_index(arr[1:],element) + 1
+    if ansfromrecursion == -1:
+        return ansfromrecursion
+    else:
+        return ansfromrecursion + 1
+
+
+arr = [3,2,1,7,5,7,6]       
+print(find_first_index(arr,5))
+
+
+    
